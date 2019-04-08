5514ece45ee0bec7592110ff682d5b2b9cff7b91
Jozef Szymala <jozef.szymala@thesoftwarehouse.pl>
2019-04-08 15:41:26 +0200

    changed template file names

--- src/handlers/versionManager.ts
+++ src/handlers/versionManager.ts
@@ -1,5 +1,5 @@
-import { version_2_6_1 } from '../../template/2.6.1/package_json';
-import { version_3_0_0 } from '../../template/3.0.0/package_json';
+import { version_2_6_1 } from '../../template/2.6.1/version_2_6_1';
+import { version_3_0_0 } from '../../template/3.0.0/version_3_0_0';
 
 const availableVersions = [version_2_6_1, version_3_0_0];
 
